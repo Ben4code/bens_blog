b6c1139e89691a63a3a1de040e07a9ccdc6194a0
nnaemeka obioha <nnaemeka.obioha@gmail.com>
2019-09-05 23:09:23 +0100

    Added navbar with reactstrap, bootstrap and sass

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,66 @@
+import React, { Component } from 'react'
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink
+} from 'reactstrap';
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+
+
+export default class Header extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar fixed="top" light expand="sm">
+          <NavbarBrand href="/">{this.props.siteTitle}</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink>
+                  <Link to="/about">About</Link>
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink>
+                  <Link to="/team">Team</Link>
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink>
+                  <Link to="/tags">Tags</Link>
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
@@ -39,4 +70,4 @@ Header.defaultProps = {
   siteTitle: ``,
 }
 
-export default Header
+
